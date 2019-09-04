55c4071659a36ad3650a497db42e18a3975c4e8e
HuangHaoqi <huang.haoq@husky.neu.edu>
2019-09-04 18:38:35 -0400

    nmsl

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app=express();
 // '/'
 //
 app.get('/',(req,res)=>{
-    res.send({bb:'Can I kiss you>?'});
+    res.send({jiaming:'wo shi sha bi'});
 });
 
 //deploy 1.port binding,Specify Node Environment, specify start script,create .gitigmore file
